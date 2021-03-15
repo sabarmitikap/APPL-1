1d40e2d9eb65dfa37c80d00f6af89afa22ac767c
sabarmitikap <sabar.itikaf62@gmail.com>
2021-03-15 11:43:40 +0700

    Inisiasi Awal
    Memasukan source code ke dalam project

---
+++ Week1/src/anothertypeofemployee/Commission.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anothertypeofemployee;
+
+/**
+ *
+ * @author N I T RO
+ */
+
+public class Commission extends Hourly {
+    private double totalSales; 
+    private double comrate;
+    
+    public Commission(String eName, String eAddress, String ePhone, String socSecNumber, double rate, double corate){
+        super(eName, eAddress, ePhone, socSecNumber, rate);
+        comrate = corate;
+    }
+    public void addSales(double newtotalSales){
+        totalSales += newtotalSales;
+    }
+    @Override
+    public double pay(){
+        totalSales = totalSales*comrate;
+        totalSales = totalSales + super.pay(); 
+        return totalSales;
+    }
+    public String toString(){
+        String result = super.toString();
+        result += "\nTotal Sales: "+ totalSales;
+        return result;
+    }
+}
---
+++ Week1/src/anothertypeofemployee/Volunteer.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anothertypeofemployee;
+
+/**
+ *
+ * @author N I T RO
+ */
+public class Volunteer extends StaffMember{
+//----------------------------------------------------------
+// Constructor: Sets up this volunteer using the specified
+// information.
+//----------------------------------------------------------
+    public Volunteer (String eName, String eAddress, String ePhone){
+        super (eName, eAddress, ePhone);
+    }
+//----------------------------------------------------------
+// Returns a zero pay value for this volunteer.
+//----------------------------------------------------------
+    public double pay(){
+        return 0.0;
+    }
+}
