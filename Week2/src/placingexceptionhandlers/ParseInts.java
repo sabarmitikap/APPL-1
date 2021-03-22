be9aaa26593fe2766f80328af0a0c74919b030d2
sabarmitikap <sabar.itikaf62@gmail.com>
2021-03-22 15:51:40 +0700

    Final

--- Week2/src/placingexceptionhandlers/ParseInts.java
+++ Week2/src/placingexceptionhandlers/ParseInts.java
@@ -4,11 +4,35 @@
  * and open the template in the editor.
  */
 package placingexceptionhandlers;
+import java.util.Scanner;
 
 /**
  *
- * @author N I T RO
+ * @author Sabar
  */
+
 public class ParseInts {
+public static void main(String[] args) {
     
+int val, sum=0;
+String line;
+Scanner scan = new Scanner(System.in);
+
+System.out.println("Enter a line of text");
+Scanner scanLine = new Scanner(scan.nextLine());
+
+while (scanLine.hasNext()) {
+val = Integer.parseInt(scanLine.next());
+sum += val;
+
+try {
+    val = Integer.parseInt(scanLine.next());
+    sum += val;
+}
+catch (Exception e) {
+    //Do Nothing
+}
+}
+System.out.println("The sum of the integers on this line is " + sum);
+}
 }
