6d39296f0edd6c6d3b33ded6d06465884c73b08e
sabarmitikap <sabar.itikaf62@gmail.com>
2021-03-24 01:05:08 +0700

    Final + Rapihin

--- Week2/src/throwingexceptions/Factorials.java
+++ Week2/src/throwingexceptions/Factorials.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package throwingexceptions;
 import java.util.Scanner;
 
@@ -12,16 +13,27 @@ import java.util.Scanner;
  */
 
 public class Factorials {
-public static void main(String[] args) {
-String keepGoing = "y";
-Scanner scan = new Scanner(System.in);
-while (keepGoing.equals("y") || keepGoing.equals("Y")) {
-System.out.print("Enter an integer: ");
-int val = scan.nextInt();
-System.out.println("Factorial(" + val + ") = " 
-        + MathUtils.factorial(val));
-System.out.print("Another factorial? (y/n) ");
-keepGoing = scan.next();
-}
+    public static void main(String[] args) {
+    String keepGoing = "y";
+    Scanner scan = new Scanner(System.in);
+    
+    while (keepGoing.equals("y") || keepGoing.equals("Y")) {
+        System.out.print("Enter an integer: ");
+        int val = scan.nextInt();
+            try {
+                if (val < 0) {
+                    throw new IllegalArgumentException("Value tidak boleh negatif");
+                }
+                else if (val > 16) {
+                    throw new IllegalArgumentException("Value tidak boleh lebih dari 16");
+                }
+                System.out.println("Factorial(" + val + ") = " + MathUtils.factorial(val));
+            }                
+            catch (IllegalArgumentException e) {
+                    throw new IllegalArgumentException("Inputan tidak boleh bernilai negatif");
+            }
+        System.out.print("Another factorial? (y/n) ");
+        keepGoing = scan.next();
+    }
 }
 }
--- Week2/src/throwingexceptions/MathUtils.java
+++ Week2/src/throwingexceptions/MathUtils.java
@@ -11,13 +11,15 @@ package throwingexceptions;
  */
 
 public class MathUtils {
+    
 //-------------------------------------------------------------
 // Returns the factorial of the argument given
 //-------------------------------------------------------------
+    
 public static int factorial(int n) {
-int fac = 1;
-for (int i=n; i>0; i--)
-fac *= i;
-return fac;
+    int fac = 1;
+    for (int i=n; i>0; i--)
+    fac *= i;
+    return fac;
 }
 }
