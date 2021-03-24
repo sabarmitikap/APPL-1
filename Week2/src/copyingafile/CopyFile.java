a8a02bd048d886504a154150f256b0f4d9bebb45
sabarmitikap <sabar.itikaf62@gmail.com>
2021-03-24 14:49:58 +0700

    Final

--- Week2/src/copyingafile/CopyFile.java
+++ Week2/src/copyingafile/CopyFile.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package copyingafile;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
 
 /**
  *
@@ -11,5 +14,26 @@ package copyingafile;
  */
 
 public class CopyFile {
-    
+    public static void main(String[] args) {
+        String keepFinding = "y";
+        Scanner scan = new Scanner(System.in);
+        
+        while (keepFinding.equals("y") || keepFinding.equals("Y")){
+            System.out.println("Enter the name of the file (must include with '.txt' : ");
+        try {
+            String fileName = new Scanner(System.in).nextLine();
+            File fileLoc = new File("D:\\Java\\" + fileName);   
+            Scanner hasil = new Scanner(fileLoc); 
+            while (hasil.hasNextLine()) {
+                    System.out.println(hasil.nextLine());
+            }
+            break;
+        }catch (FileNotFoundException e){
+            System.out.print("File does not exist\n");
+        }
+        
+        System.out.print("Wanna keep looking? (y/n) ");
+        keepFinding = scan.next();
+        }
+    }
 }
